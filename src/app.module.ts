88be564fefb5cdd8671327ab1fd9f2f2d1f609c3
ayranoliveira1 <ayranoliveira1@gmail.com>
2026-04-01 19:42:01 -0300

    feat(app): configure app module with database, env, throttler, helmet and cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { ThrottlerModule } from '@nestjs/throttler'
+import { DatabaseModule } from '@/shared/database/database.module'
+import { EnvModule } from '@/shared/env/env.module'
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 300,
+        },
+      ],
+    }),
+    EnvModule,
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { AllExceptionsFilter } from '@/shared/filters/nest-exception-filter'
+import helmet from 'helmet'
+import { Logger } from '@nestjs/common'
 
 async function bootstrap() {
+  const logger = new Logger('Main')
+
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+
+  app.use(helmet())
+  app.useGlobalFilters(new AllExceptionsFilter())
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Platform'],
+  })
+
+  const port = process.env.PORT ?? 3333
+  await app.listen(port)
+  logger.log(`Application is running on: http://localhost:${port}`)
 }
 bootstrap()
