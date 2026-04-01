8ec92edeecf7f50a283fba71dd834e56f0670daf
ayranoliveira1 <ayranoliveira1@gmail.com>
2026-04-01 20:12:07 -0300

    fix(core): enforce fixed status codes 403 and 500 on NotAllowedError and UnexpectedError

--- src/core/errors/errors/not-allowed-error.ts
+++ src/core/errors/errors/not-allowed-error.ts
@@ -1,7 +1,10 @@
 import { UseCaseError, UseCaseErrorProps } from '../use-case-error'
 
 export class NotAllowedError<T> extends UseCaseError<T> {
-  constructor(props: UseCaseErrorProps<keyof T>) {
-    super(props)
+  constructor(props: Omit<UseCaseErrorProps<keyof T>, 'statusCode'>) {
+    super({
+      statusCode: 403,
+      errors: props.errors,
+    })
   }
 }
--- src/core/errors/errors/unexpected-error.ts
+++ src/core/errors/errors/unexpected-error.ts
@@ -1,7 +1,10 @@
 import { UseCaseError, UseCaseErrorProps } from '../use-case-error'
 
 export class UnexpectedError<T> extends UseCaseError<T> {
-  constructor(props: UseCaseErrorProps<keyof T>) {
-    super(props)
+  constructor(props: Omit<UseCaseErrorProps<keyof T>, 'statusCode'>) {
+    super({
+      statusCode: 500,
+      errors: props.errors,
+    })
   }
 }
