c08847fef3ac3fd6efcd2f98b1e2401c477d88bf
ayranoliveira1 <ayranoliveira1@gmail.com>
2026-04-02 12:25:29 -0300

    feat(identity): add jwt auth guard with public route decorator and get-user extractor

---
+++ src/modules/identity/infra/decorators/get-user.decorator.ts
@@ -0,0 +1,14 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common'
+
+export interface UserPayload {
+  userId: string
+  family: string
+}
+
+export const GetUser = createParamDecorator(
+  (data: keyof UserPayload | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest()
+    const user = request.user as UserPayload
+    return data ? user[data] : user
+  },
+)
